e161173a273657f2a9ac43997e3ae2783dd74ade
Alistair McCann <allyjmccann@gmail.com>
2024-09-24 11:24:29 +0200

    added more general stuff

---
+++ shui-frontend/src/components/postBtn/PostBtn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PostBtn() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PostBtn
---
+++ shui-frontend/src/components/userBtn/UserBtn.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserBtn() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UserBtn
