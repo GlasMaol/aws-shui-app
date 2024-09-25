35bd5724cb19dc27f30d6d1028a0df4db3f46eaf
Alistair McCann <allyjmccann@gmail.com>
2024-09-25 09:26:16 +0200

    added link to post btn

--- shui-frontend/src/components/footerHome/FooterHome.jsx
+++ shui-frontend/src/components/footerHome/FooterHome.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import '../footerHome/footerHome.css';
 import sendMessageBtn from '../../assets/send-message-btn.svg';
+import { Link } from 'react-router-dom';
 
 function FooterHome() {
     return (
         <div className='footer'>
-            <button className='send-message-btn'>
+            <Link to='/post' className='send-message-btn'>
                 <img src={sendMessageBtn} alt='Send mesage' />
-            </button>
+            </Link>
         </div>
     )
 }
